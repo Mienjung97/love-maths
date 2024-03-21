9d85dca5a30f70a90f30d2f3699d6198efd2dbb3
Sebastian Kefer <sebastian.kefer@gmail.com>
2024-03-21 18:24:30 +0000

    Add division game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,6 +49,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         // throw is for showing error in console
@@ -94,6 +96,9 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        // Math.round() to get integers instead of floats / makes only rounded answers possible
+        return [Math.round(operand1 / operand2), "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -138,6 +143,10 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 } 
 
-function displayDivisionQuestion() {
+function displayDivisionQuestion(operand1, operand2) {
+
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "/";  
     
 } 
